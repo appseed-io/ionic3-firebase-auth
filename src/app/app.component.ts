41a6d65091b4c7a0a8176d53d544523bb2d8b012
George Konispoliatis <konispoliatisg@gmail.com>
2018-01-24 19:56:09 +0200

    Add login and logout menu items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,6 +47,18 @@ export class MyApp {
 		this.rootPage = LoginPage;
 	}
 
+	login() {
+		this.menu.close();
+		this.auth.signOut();
+		this.nav.setRoot(LoginPage);
+	}
+
+	logout() {
+		this.menu.close();
+		this.auth.signOut();
+		this.nav.setRoot(HomePage);
+	}
+
 	openPage(page) {
 	this.menu.close();
 	this.nav.setRoot(page.component);
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -28,4 +28,16 @@ export class AuthService {
 		return this.afAuth.auth.createUserWithEmailAndPassword(credentials.email,credentials.password);
 	}
 
+	get authenticated(): boolean {
+		return this.user !== null;
+	}
+
+	getEmail() {
+		return this.user && this.user.email;
+	}
+
+	signOut(): Promise<void> {
+		return this.afAuth.auth.signOut();
+	}
+
 }
