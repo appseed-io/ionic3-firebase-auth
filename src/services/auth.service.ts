f0e8dc0b3f6140958c99a0a705f5bf8640af9bbf
George Konispoliatis <konispoliatisg@gmail.com>
2018-01-24 20:11:49 +0200

    fix(auth.service):Duplicate function signInWithEmail

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -19,11 +19,6 @@ export class AuthService {
 			 credentials.password);
 	}
 
-  signInWithEmail(credentials) {
-	   console.log('Sign in with email');
-	    return this.afAuth.auth.signInWithEmailAndPassword(credentials.email,credentials.password);
-  }
-
 	signUp(credentials) {
 		return this.afAuth.auth.createUserWithEmailAndPassword(credentials.email,credentials.password);
 	}
